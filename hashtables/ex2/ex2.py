a53865fb9e4dd7dd254006e2d3964c7b4854ba21
dueka <ekanemd@live.com>
2020-05-08 09:57:24 +0100

    completed ex 2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,18 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    
+    cache = {}
+    route = [None] * length
     """
     YOUR CODE HERE
     """
+    for ticket in tickets:
+        cache.update(ticket.source, ticket.destination)
+    first_ticket = cache.get('None')
+    route[0] = first_ticket
 
+    for i in range(1, len(route)):
+        prev_ticket = route[i - 1]
+        prev_ticket_key = cache.get(prev_ticket)
+        route[i] = prev_ticket_key
     return route
