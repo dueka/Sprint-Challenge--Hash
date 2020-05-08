c47c21f71ddd6f34076bb47ea55e52b273fc4e1f
dueka <ekanemd@live.com>
2020-05-08 10:04:27 +0100

    Removed the length from the route

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -7,17 +7,20 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     cache = {}
-    route = [None] * length
+    route = []
     """
     YOUR CODE HERE
     """
-    for ticket in tickets:
-        cache.update(ticket.source, ticket.destination)
-    first_ticket = cache.get('None')
-    route[0] = first_ticket
-
-    for i in range(1, len(route)):
-        prev_ticket = route[i - 1]
-        prev_ticket_key = cache.get(prev_ticket)
-        route[i] = prev_ticket_key
+    for index, ticket in enumerate(tickets):
+        cache[ticket.source] = ticket.destination
+    first_ticket = cache['NONE']
+    # route[0] = first_ticket
+    while first_ticket is not 'NONE':
+        route.append(first_ticket)
+        first_ticket = cache[first_ticket]
+    route.append(first_ticket)
+    # for i in range(1, len(route)):
+    #     prev_ticket = route[i - 1]
+    #     prev_ticket_key = cache.get(prev_ticket)
+    #     route[i] = prev_ticket_key
     return route
