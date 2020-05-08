152e692e64e6be66465d4e568ebc3a5c8bf00f8b
dueka <ekanemd@live.com>
2020-05-08 08:44:39 +0100

    added the dict to ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,33 @@
 def get_indices_of_item_weights(weights, length, limit):
+    cache = {}
 
     """
     YOUR CODE HERE
     """
+    for i in range(0, length):
+        current_weight = weights[i]
+        (cache, current_weight, i)
+        diff = limit - current_weight
+        result = (cache, diff)
+        if result:
+            if length == 2:
+                return (1, 0)
+            return (i, result)
+        return None
 
-    return None
+
+def get_check(check):
+    if check is not None:
+        print(str(check[0] + "_" + check[1]))
+    else:
+        print("None")
+
+
+# >> > x = {1: 2}
+# >> > print(x)
+# {1: 2}
+
+# >> > d = {3: 4, 5: 6, 7: 8}
+# >> > x.update(d)
+# >> > print(x)
+# {1: 2, 3: 4, 5: 6, 7: 8}
